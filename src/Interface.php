34d8688a7ffa3c1f85853a41894b17d3658e0ff0
Kandar <k4ndar@yahoo.com>
2015-07-11 17:36:08 +0700

    Initial commit

---
+++ src/Interface.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Panada\Session\Interfaces;
+
+/**
+ * Interface for Session Drivers
+ *
+ * @package  Session
+ * @author   Iskandar Soesman <k4ndar@yahoo.com>
+ * @link     http://panadaframework.com/
+ * @license  http://www.opensource.org/licenses/bsd-license.php
+ * @since    version 1.0.0
+ */
+interface Session
+{
+    public function setValue($name, $value = '');
+    public function getValue($name);
+    public function getAllValue();
+    public function deleteValue($name);
+    public function regenerateId();
+    public function destroy();
+}
